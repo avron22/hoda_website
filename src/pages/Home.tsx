2fe44325e949e522db905e85bf12b3f406a776b2
IPham23 <iancoladilla23@gmail.com>
2025-11-03 15:51:12 +0800

    Update Home.tsx and Navbar.tsx
    finish the layout for all breakpoints in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,25 +4,51 @@ export default function Home(){
 
   return (
     <>
-      <div className="hero-bg h-[100vh] flex flex-col justify-between max-h-[100vh] overflow-hidden sm:px-[30px] sm:py-0 xl:px-[140px] xl:pt-[60px] xl:pb-[90px]"> 
-        <section>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        {/*Mobile Navber */}
+        <div className="lg:hidden sm:px-9 md:px-20">
           <Navbar />
-          <div>
-            <h3 className="max-w-[420px] text-end text-2xl font-light">RUNWAY & FASHION MODEL</h3>
-            <div className="absolute top-3/7 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between w-full text-6xl font-medium tra slate-y-1/3 pl-[360px] pr-[440px]">
-              <NavLink to="/models/runway-model" className="">RUNWAY</NavLink>
-              <NavLink to="/models/fashion-model" className="">MODEL</NavLink>
+        </div>
+        <div className="hero-bg flex-1 lg:h-[100vh] flex flex-col justify-between lg:max-h-[100vh] overflow-hidden 
+                        sm:px-[20px] sm:py-0 md:max-lg:justify-center md:px-[60px] 
+                        lg:px-[100px] lg:pt-[40px] lg:pb-[60px]
+                        xl:px-[140px] xl:pt-[60px] xl:pb-[90px]"> 
+          <section>
+        {/*Desktop Navbar*/}
+            <div className="sm:max-lg:hidden">
+              <Navbar />
             </div>
-          </div>
-        </section>
-        <section>
-          <div className="flex items-center justify-end gap-20 pr-10">
-            <a href="/"><img src="/src/assets/ICONS/TIKTOK_ICON.svg" alt="TIKTOK" /></a>
-            <a href="/"><img src="/src/assets/ICONS/FB_ICON.svg" alt="FACEBOOK" /></a>
-            <a href="/"><img src="/src/assets/ICONS/IG_ICON.svg" alt="INSTAGRAM" /></a>
-            <a href="/"><img src="/src/assets/ICONS/EMAIL_ICON.svg" alt="EMAIL" /></a>
-          </div>
-        </section>
+        {/*Contents */}
+            <div>
+              <h3 className="max-w-[420px] text-end text-2xl font-light sm:max-lg:hidden">RUNWAY & FASHION MODEL</h3>
+              <div className="sm:max-lg:text-[30px] sm:max-md:pt-3
+                              lg:absolute lg:top-3/7 lg:left-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2 
+                              flex justify-between lg:w-full lg:text-6xl font-medium 
+                              lg:pl-[160px] lg:pr-[200px]
+                              xl:pl-[360px] xl:pr-[440px]">
+                <NavLink to="/models/runway-model" className="">RUNWAY</NavLink>
+                <NavLink to="/models/fashion-model" className="">MODEL</NavLink>
+              </div>
+            </div>
+          </section>
+          <section className="sm:max-lg:hidden">
+            <div className="flex items-center justify-end gap-20 pr-10">
+              <a href="/"><img src="/src/assets/ICONS/TIKTOK_ICON.svg" alt="TIKTOK" /></a>
+              <a href="/"><img src="/src/assets/ICONS/FB_ICON.svg" alt="FACEBOOK" /></a>
+              <a href="/"><img src="/src/assets/ICONS/IG_ICON.svg" alt="INSTAGRAM" /></a>
+              <a href="/"><img src="/src/assets/ICONS/EMAIL_ICON.svg" alt="EMAIL" /></a>
+            </div>
+          </section>
+        </div>
+        {/*Mobile Footer */}
+        <div className="py-[10px] lg:hidden">
+            <div className="flex items-center justify-center gap-[50px] px-[50px]">
+              <a href="/"><img src="/src/assets/ICONS/TIKTOK_FILL_ICON.svg" className="w-[25px]" alt="TIKTOK" /></a>
+              <a href="/"><img src="/src/assets/ICONS/FB_FILL_ICON.svg" className="w-[25px]" alt="FACEBOOK" /></a>
+              <a href="/"><img src="/src/assets/ICONS/IG_FILL_ICON.svg" className="w-[25px]" alt="INSTAGRAM" /></a>
+              <a href="/"><img src="/src/assets/ICONS/EMAIL_FILL_ICON.svg" className="w-[25px]" alt="EMAIL" /></a>
+            </div>
+        </div>
       </div>
     </>
   );
